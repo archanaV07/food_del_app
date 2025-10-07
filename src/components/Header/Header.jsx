c2a7eac1a3aa9e78d1d5727275ccbd79c34730e6
Archana Vishwanath <archanavishwanath06@gmail.com>
2025-10-07 10:58:28 +0530

    header_image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Header.css'
+import header_Img from '/assets/header_img.png' ;
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header'
+    style={{
+      backgroundImage: `url(${header_Img})`,
+      backgroundSize: 'contain',
+      backgroundRepeat: 'no-repeat',  
+      }}
+      >
       <div className="header-contents">
       <h2> Order your food here! </h2>
       <p>Delicious food, endless choices, and lightning-fast delivery — because hunger can’t wait</p>
