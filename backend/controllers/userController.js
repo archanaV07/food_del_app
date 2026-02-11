ea47c09bc62e97575d9f64e846500f1ddccd9506
Archana Vishwanath <archanavishwanath06@gmail.com>
2026-02-11 21:26:25 +0530

    login_register_api_backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,73 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"; 
+import validator from "validator";
+
+//login User
+const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({success:false, message:"User not found"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.json({success:false, message:"Invalid credentials"})
+        }
+
+        const token = createToken(user._id);    
+        res.json({success:true, message:"User logged in successfully", token:token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error in logging in user"})
+    }
+}
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
+    //register User
+
+const registerUser = async (req,res) => {
+    const {name, email, password} = req.body;
+    try {
+        // checking if user already exists
+            const exists = await userModel.findOne({email});
+            if(exists) {
+                return res.json({success:false, message:"User already exists"})
+            }
+        
+        // validating email format and strong password
+        if (!validator.isEmail(email)){
+            return res.json({success:false, message:"Please enter a valid email"})
+        }
+
+        if(password.length < 8) {     
+            return res.json({success:false, message:"Please enter a strong password"})
+        }
+
+        // hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // creating new user
+        const newUser = new userModel({
+            name:name, 
+            email:email,
+            password:hashedPassword
+        })
+        const user = await newUser.save()
+        const token = createToken(user._id);
+        res.json({success:true, message:"User registered successfully", token:token})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error in registering user"})
+
+}
+}
+export {loginUser,registerUser}
