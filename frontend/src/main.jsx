6966211a22571761df0b4b12ccab8ae8c5cbe45f
Archana Vishwanath <archanavishwanath06@gmail.com>
2026-02-12 13:28:32 +0530

    food_rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode, React } from 'react'
-import { createRoot, ReactDOM } from 'react-dom/client'
+import React from 'react'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import  StoreContextProvider  from './context/StoreContext.jsx';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
   <BrowserRouter>
     <StoreContextProvider>
